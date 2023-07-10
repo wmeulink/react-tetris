48670689d1cf1f1bad3273d33723033e1b1123ea
Whitney Meulink <wmeulink@gmail.com>
2023-07-09 18:03:23 -0700

    adding about page, and routing

--- src/Home/About.js
+++ src/Home/About.js
@@ -1,10 +1,38 @@
+import './About.css';
+
 function About() {
     return (
-      <div style={{ padding: 20 }}>
-        <h2>About View</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-      </div>
+        <div>
+            <div className="about-me-header">
+                Hi, I'M
+            </div>
+            <div className="about-me-name">
+                Whitney Elliott
+            </div>
+            <div />
+            <div className='body-container'>
+            <div className="about-me-body-column1">
+                <div className="about-body-title">
+                    A Little about me
+                </div>
+                <p className='about-me-body-text'>
+                    Hi there! I'm Whitney Elliott, a passionate Software Engineer hailing from the beautiful Pacific Northwest. With a strong focus on Typescript, React, ASP.NET Core, and ASP.NET MVC, I thrive in the ever-evolving world of software development.
+                </p>
+                <p className='about-me-body-text'>
+                The bond I share with my Labrador Retrievers goes beyond companionship. We embark on countless adventures together, exploring the scenic trails of the Pacific Northwest, enjoying the outdoors, and making memories that will last a lifetime. Their zest for life and infectious enthusiasm remind me to live in the present and appreciate the simple pleasures that nature offers.
+                </p>
+                <p className='about-me-body-text'>
+                As I continue to navigate my professional journey as a Software Engineer, my Labrador Retrievers serve as a constant reminder of the importance of balance and finding joy in the little things. They have taught me the value of patience, empathy, and the power of a wagging tail to brighten even the darkest of days.
+                </p>
+            </div>
+            <div className="about-me-body-column2">
+                <img className="image1" src={ require('./bandw.png') } />
+                <img className="image2" src={ require('./canbyhouse.jpg')} />
+            </div>
+            </div>
+
+        </div>
     );
-  }
+}
 
 export default About;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,47 +1,60 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './About';
+import Header from './Header';
 import Tetris from '../components/Tetris';
-
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-}
+import './Header.css';
 
 function NoMatch() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>404: Page Not Found</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
+    return (
+        <div style={{ padding: 20 }}>
+            <h2>404: Page Not Found</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        </div>
+    );
 }
 
 function App() {
-  return (
-    <Router>
-        <nav style={{ margin: 10 }}>
-          <Link to="/" style={{ padding: 5 }}>
-          Home
-          </Link>
-          <Link to="/about" style={{ padding: 5 }}>
-          About
-          </Link>
-          <Link to="/tetris" style={{ padding: 5}}>
-          Tetris
-          </Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Tetris" element={<Tetris />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <div>
+            <div>
+                <Router>
+                    <div className="router-header-container">
+                    <div className='header-container'>
+                        <Header />
+                    </div>
+                    <nav className='nav-container'>
+                        <Link to="/" className="link-styles">
+                            Home
+                        </Link>
+                        <Link to="/about" className="link-styles">
+                            About
+                        </Link>
+                        <Link to="/tetris" className="link-styles">
+                            Tetris
+                        </Link>
+                    </nav>
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/About" element={<About />} />
+                        <Route path="/Tetris" element={<Tetris />} />
+                        <Route path="*" element={<NoMatch />} />
+                    </Routes>
+                </Router>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+    );
+}
+
+function Home() {
+    return (
+        <div></div>
+    );
 }
 
 export default App;
